5cece328437db44831f3b0e141398a77a24ccb78
Lyos Anatoliy <les.anatoliy.92@gmail.com>
2019-10-14 23:47:56 +0600

    minor changes

--- resources/views/links/show.blade.php
+++ resources/views/links/show.blade.php
@@ -5,7 +5,7 @@
 		<div class="card-header">
 			<i class="fa fa-align-justify"></i> Просмотр : {{ $link->title_ru }}
 			<div class="card-actions">
-				<a href="{{ route('adminlinks::sections.links.index', ['id' => $id]) }}" class="btn btn-default pl-3 pr-3" style="width: 70px;" title="Назад"><i class="fa fa-arrow-left"></i></a>
+				<a href="{{ url()->previous() }}" class="btn btn-default pl-3 pr-3" style="width: 70px;" title="Назад"><i class="fa fa-arrow-left"></i></a>
 			</div>
 		</div>
 		<div class="card-body">
